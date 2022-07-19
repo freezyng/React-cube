b6adc24efde120f472cda741c26dc13c1c16039c
ershov_as <ershov_as@teleformis.ru>
2022-07-19 16:36:40 +0300

    first commit

---
+++ src/components/cube.jsx
@@ -0,0 +1,29 @@
+
+export const Cube = () => {
+  return (
+    <>
+      <div id="wrapD3Cube">
+        <div id="D3Cube">
+          <div id="side1">
+            <img className="cube-skills-img" src="img/unnamed.jpg" alt="img"/>
+          </div>
+          <div id="side2">
+            <img className="cube-skills-img" src="img/493753d436221fb7.jpg" alt="img"/>
+          </div>
+          <div id="side3">
+            <img className="cube-skills-img" src="img/5337774.png" alt="img"/>
+          </div>
+          <div id="side4">
+            <img className="cube-skills-img" src="img/5370574.png" alt="img"/>
+          </div>
+          <div id="side5">
+            <img className="cube-skills-img" src="img/GqA0dn1_1ls.jpg" alt="img"/>
+          </div>
+          <div id="side6">
+            <img className="cube-skills-img" src="img/1537798471.645701-.jpg" alt="img"/>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
